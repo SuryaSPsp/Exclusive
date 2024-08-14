0ae7b6efb55b563dc398c6369a9c6d9666cae1c7
SuryaSPsp <142131790+SuryaSPsp@users.noreply.github.com>
2024-08-14 12:24:55 +0530

    Update NextPage.jsx

--- src/components/NextPage.jsx
+++ src/components/NextPage.jsx
@@ -91,16 +91,3 @@ export const NextPage = () => {
 };
 
 
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// export const NextPage = () => {
-
-//   return (
-//     <>
-//         <div className="prod-btm">
-//             <Link to="/product">View All Products</Link>
-//         </div>
-//     </>
-//   )
-// }
