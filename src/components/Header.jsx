c18456fd5f7cefccac910e5f771f3567fb537566
SuryaSPsp <142131790+SuryaSPsp@users.noreply.github.com>
2024-08-14 12:23:08 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -94,19 +94,3 @@ export const Header = () => {
 };
 
 
-// import React from 'react'
-
-
-// export const Header = () => {
-//   return (
-//     <>
-//     <div className="nav-1">
-//         <p>Summer Sale For All Swim Suits And Free Express Delivery- OFF 50%! <span>Shop Now</span> </p>
-//         <select>
-//             <option>English</option>
-//             <option>Tamil</option>
-//         </select>
-//     </div>
-//     </>
-//   )
-// }
